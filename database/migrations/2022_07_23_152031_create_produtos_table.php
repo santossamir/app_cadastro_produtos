7a0e836963b878b6b5ba3672031d9a2f97c76bd1
Samir Santos <santossamir@hotmail.com>
2022-07-23 12:22:31 -0300

    Adicionando e relacionando as tableas no banco de dados.

---
+++ database/migrations/2022_07_23_152031_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_produto', 100);
+            $table->string('preco_produto', 100);
+            $table->unsignedBigInteger('categoria_id');
+            $table->timestamps();
+
+            //constraint de relacionamento
+           $table->foreign('categoria_id')->references('id')->on('categorias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
